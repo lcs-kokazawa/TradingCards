63d4d2ad1ef6c19ad9bf1091f15363be7c7b3397
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-01-12 15:11:50 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by Kiho Okazawa on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
